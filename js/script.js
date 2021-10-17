30e707400b508311c9eb7cda80a5dd35ab874a72
omar96developer <omakino@hotmail.it>
2021-10-17 18:48:40 +0200

    3.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -22,47 +24,22 @@ const text = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
 
-
-  
-const Container = document.querySelector('.img-box');
-const Container2 = document.querySelector('.text-img');
+const sezioneSx = document.querySelector(".sezione-sx");
+console.log(sezioneSx)
 
 
 
-
-let currentIndex = 0;
-
-/* for (let i = 0; i < items.length; i++) {
-
-    const imgContainer = items[i]; 
-    const textimg = title[i]
-
+for(i = 0; i < 5; i++) {
+    const img = items[i]
+    const tit = title[i]
+    const txt = text[i]
     let classUsare ='';
 
     if( i === 0){
         classUsare= 'active';
     }
-    
-    const tagimg = `<img  class="${classUsare}" src="${imgContainer}" alt="immagine${i}">`;
-    const testo = `<div class="text-img"><p>${textimg}</p></div>`
-    Container.innerHTML += tagimg + testo;
-} */
-for (let i = 0; i < items.length; i++) {
-
-    const imgContainer = items[i]; 
-    
-
-    let classUsare ='';
-
-    if( i === 0){
-        classUsare= 'active';
-    }
-    
-    const tagimg = `<img  class="${classUsare}" src="${imgContainer}" alt="immagine${i}">`;
-    
-    Container.innerHTML += tagimg;
-
-    
+    const div = `<div class="contenitore-immagine ${classUsare}"><img src="${img}" alt="0"><div class="testo"><h3 class="titolo-immagine">${tit}</h3><p class="descrizione-immagine">${txt}</p></div></div>`
+    sezioneSx.innerHTML += div
 }
 
 //attivazioni pulsanti su giu
@@ -70,30 +47,12 @@ for (let i = 0; i < items.length; i++) {
 const btnUp = document.querySelector('.btn-up');
 const btnDown = document.querySelector('.btn-down');
 
-btnUp.addEventListener("click", function () {
 
-    const activeImage = document.querySelector(".active");
-    activeImage.classList.remove("active");
-    currentIndex--;
-    if (currentIndex < 0) {
-        currentIndex = items.length - 1;
-      };
-    const imgTags = Container.querySelectorAll("img");
-    const newActiveImage = imgTags[currentIndex];
-    newActiveImage.classList.add("active");
-    
-});
 
 btnDown.addEventListener("click", function () {
+    const active = document.querySelector('.active')
+    active.classList.remove('active')
+    active.nextElementSibling.classList.add('active')
 
-    const activeImage = document.querySelector(".active");
-    activeImage.classList.remove("active");
-    currentIndex++;
-    if (currentIndex > items.length - 1) {
-        currentIndex = 0;
-      }
-    const imgTags = Container.querySelectorAll("img");
-    const newActiveImage = imgTags[currentIndex];
-    newActiveImage.classList.add("active");
-    
-});
+}); 
+ 
